d02b903a6d74063e7c0531dc0c5657a94b3488f4
Zimplay <106824103+Zimplay@users.noreply.github.com>
2023-10-27 14:03:57 +0300

    Update DataBase.py

--- programmer/DataBase.py
+++ programmer/DataBase.py
@@ -3,10 +3,14 @@ from mysql.connector import connect, Error
 
 try:
     with connect(
-        host = 'localhost',
+        host = '62.76.220.88',
         user = 'root',
         password = 'Jkbvg!18'
     ) as connection:
-        print(connection)
+        with connection.cursor() as cursor:
+            cursor.execute('SHOW DATABASES')
+            for db in cursor:
+                print(db)
 except Error as e:
     print(e)
+
